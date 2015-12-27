d311d4b79334bd1ab19f7a4659ec7b5b3f5f8873
Brokenprogrammer <brokenprogrammer@gmail.com>
2015-12-27 01:46:25 +0100

    Attempt to implement channels

--- src/controlpanel/controlpanel.go
+++ src/controlpanel/controlpanel.go
@@ -3,22 +3,37 @@ package controlpanel
 import (
 	"fmt"
 	"net"
+	//"time"
 	"watcher"
 )
 
 //Experimenting with a ControlPanel for the bot.
-func ControlPanel(conn net.Conn, bot *watcher.Watcher) {
+func ControlPanel(conn net.Conn, bot *watcher.Watcher, c chan string) {
 	for {
+		//msg := <-c
+
+		/*switch msg {
+		default:
+			conn.Write([]byte("PRIVMSG " + bot.Channel + " :" + msg + "\r\n"))
+		}*/
+
+		/*select {
+		case msg := <-c:
+			fmt.Println("Do work!", msg)
+		case <-time.After(time.Millisecond * 1):
+			fmt.Println("Def")
+		}*/
+
 		fmt.Println("Waiting for input: ")
+		var input string //Input variable to hold input
 
-		var input string
+		fmt.Scan(&input) //Scan the input to read what was typed
 
-		fmt.Scan(&input)
-
+		//Actions depending on scanned input
 		switch input {
-		case "Hello":
+		case "Hello": //Print hello message
 			conn.Write([]byte("PRIVMSG " + bot.Channel + " :" + hello()))
-		case "Quit":
+		case "Quit": //Quit
 			conn.Write([]byte("QUIT " + "\r\n"))
 		}
 	}
--- src/main.go
+++ src/main.go
@@ -11,8 +11,15 @@ import (
 )
 
 func main() {
+	c := make(chan string)
+
+	//Our Bot Instance
 	bot := watcher.NewBot()
+
+	//Connection instance copy
 	conn, _ := bot.Connect()
+
+	//defer statement to close the connection when this function ends
 	defer conn.Close()
 
 	//db.InitDB()
@@ -24,7 +31,7 @@ func main() {
 	conn.Write([]byte("PRIVMSG " + bot.Channel + " :Hello World!\r\n"))
 
 	//Using a Go Routine to handle a Control Panel for the bot simultaniously as the bot is running
-	go controlpanel.ControlPanel(conn, bot)
+	go controlpanel.ControlPanel(conn, bot, c)
 
 	//The bufio reader will read data we get from our connection and return it as a string.
 	connBuff := bufio.NewReader(conn)
@@ -41,6 +48,11 @@ func main() {
 			if splitted[0] == "PING" {          //If the IRC Server is pinging us
 				conn.Write([]byte("PONG " + splitted[1] + "\r\n"))                            //Respond back with a PONG
 				conn.Write([]byte("PRIVMSG " + bot.Channel + " :Hello I'm Still here! \r\n")) //Tell the chat you're still here
+			}
+
+			//Sending the string through channel to our controlpanel
+			if splitted[1] == "PRIVMSG" {
+				//c <- str
 			}
 		}
 		if err != nil {
