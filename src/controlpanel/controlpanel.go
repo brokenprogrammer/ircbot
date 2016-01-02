475b9a2f5a94d317d91b35a9b0392594e732c46d
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-01-02 19:06:51 +0100

    Commented issue #12, Resolved #12

--- src/controlpanel/controlpanel.go
+++ src/controlpanel/controlpanel.go
@@ -64,14 +64,22 @@ func statusCommand(bot *watcher.Watcher, conn net.Conn, DBConn *db.Crud) {
 	conn.Write([]byte("PRIVMSG " + bot.Channel + " :" + "BrokenBot Status: Uptime: " + time.Since(bot.RanFor).String() + " Tracking: " + strconv.Itoa(db.GetAmmountOfUsers(DBConn)) + " users and tracking: " + strconv.Itoa(db.GetAmmountOfMessages(DBConn)) + " messages. \r\n"))
 }
 
+//evalCommand is a command function called by readMessage incase a user
+//Requested a statement to be evaluated
 func evalCommand(bot *watcher.Watcher, conn net.Conn, message string) {
+	//Use the formatter library to extract the message from the user input
 	newMess := formatter.ExtractMessage(message)
+
+	//Using token and ast librariies to parse the string reading one value at the time.
 	fs := token.NewFileSet()
 	tr, _ := parser.ParseExpr(newMess[6:])
 
+	//string to hold all the expressions
 	var expression []string
-	//var expressionString string
+
+	//The used interface to hold all the values typed in the eval statement
 	var Evalvalues []interface{}
+	//The used interface to hold all the operators typed in the eval statement
 	var operands []interface{}
 	ast.Print(fs, tr)
 	//Gets the different values and what position they are at. Find a way to find the operators as well.
@@ -97,40 +105,49 @@ func evalCommand(bot *watcher.Watcher, conn net.Conn, message string) {
 		return true
 	})
 
+	//Check if float or string should be used
 	useFloat, useString := checkType(Evalvalues)
-	var floatSum float64
-	var stringSum string
+	var floatSum float64 //variable to be used if float returns true
+	var stringSum string //variable to be used if string returns true
 
 	fmt.Println("useFloat: ", useFloat, " useString: ", useString)
 
+	//Actions depending on if we use string or float
 	if useString == true {
+		//Looping through all values
 		for i, _ := range Evalvalues {
+			//If its not the first loop
 			if i != 0 {
 				operator := operands[i-1]
+				//Check which operator is used
 				switch operator {
 				case "+":
 					stringSum += Evalvalues[i].(string)
 				}
-			} else {
+			} else { //If its the first loop then just add the first value
 				stringSum += Evalvalues[i].(string)
 			}
 		}
 	} else if useFloat == true {
+		//Looping through all the values
 		for i, _ := range Evalvalues {
+			//If its no the first loop
 			if i != 0 {
 				operator := operands[i-1]
+				//Check which operatis is used
 				switch operator {
 				case "+":
 					tempFloat, _ := strconv.ParseFloat(Evalvalues[i].(string), 64)
 					floatSum += tempFloat
 				}
-			} else {
+			} else { //If its the first loop then add the first value
 				tempFloat, _ := strconv.ParseFloat(Evalvalues[i].(string), 64)
 				floatSum += tempFloat
 			}
 		}
 	}
 
+	//Write to the chat depending on if its a string or float used.
 	if useString == true {
 		conn.Write([]byte("PRIVMSG " + bot.Channel + " :" + newMess[6:len(newMess)-2] + ": " + stringSum + " \r\n"))
 	} else if useFloat == true {
@@ -143,6 +160,7 @@ func testReturnNum(s string) int {
 	return a
 }
 
+//Check which types are included, returns 2 booleans, one for string and one for float
 func checkType(i []interface{}) (bool, bool) {
 	useFloat := false
 	useString := false
